b08f69e2b8d285f7fc735766940560b6b3010dba
Edvin Ardö <edvin.ardo@bontouch.com>
2024-01-15 21:19:27 +0100

    Add router links to routes

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { routes } from "./router";
+</script>
+
 <template>
+  <div class="routes">
+    <div v-for="route in routes">
+      <RouterLink :to="route.path">{{ route.title }}</RouterLink>
+    </div>
+  </div>
   <RouterView />
 </template>
+
+<style scoped lang="scss">
+.routes {
+  display: flex;
+  gap: 8px;
+
+  :any-link {
+    color: white;
+  }
+}
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./components/Home.vue";
 
-const routes = [{ path: "/", component: Home }];
+export const routes = [{ path: "/", title: "Home", component: Home }];
 
 export const router = createRouter({
   history: createWebHistory(),
