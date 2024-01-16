10e38ac4d001e63870d5580cfc602d780ff7732c
Edvin Ardö <edvin.ardo@bontouch.com>
2024-01-16 11:06:01 +0100

    Add sub routing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import { useRoute } from "vue-router";
+import { computed } from "vue";
 import { routes } from "./router";
+
+const currRoute = useRoute();
+
+const rootPath = computed(() => `/${currRoute.fullPath.split("/")[1]}`);
+
+const subRoutes = computed(
+  () => routes.find((route) => route.path === rootPath.value)?.children
+);
 </script>
 
 <template>
   <div class="routes">
     <div v-for="route in routes">
-      <RouterLink :to="route.path">{{ route.title }}</RouterLink>
+      <RouterLink :to="route.path">{{ route.name }}</RouterLink>
+    </div>
+  </div>
+  <div class="routes">
+    <div v-for="subRoute in subRoutes">
+      <RouterLink :to="`${rootPath}/${subRoute.path}`">
+        {{ subRoute.name }}
+      </RouterLink>
     </div>
   </div>
   <RouterView />
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./components/Home.vue";
-import Reverse from "./components/Reverse.vue";
-import ReverseBuggy from "./components/ReverseBuggy.vue";
+import ReverseFix from "./components/ReverseFix.vue";
+import ReverseProblem from "./components/ReverseProblem.vue";
 
 export const routes = [
-  { path: "/", title: "Home", component: Home },
-  { path: "/reverse-buggy", title: "ReverseBuggy", component: ReverseBuggy },
-  { path: "/reverse", title: "Reverse", component: Reverse },
+  { path: "/", name: "Home", component: Home },
+  {
+    path: "/reverse",
+    name: "column-reverse",
+    children: [
+      { path: "", name: "Problem", component: ReverseProblem },
+      { path: "fix", name: "Fix", component: ReverseFix },
+    ],
+  },
 ];
 
 export const router = createRouter({
